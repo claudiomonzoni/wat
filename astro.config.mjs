a87cba522853731750387a7e5427bfc1bc3da658
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-08-04 08:17:41 -0600

    quitando las transiciones, no funcionan los scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
-import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), swup()]
+  // experimental: {
+  //   viewTransitions: true
+  //  },
+  integrations: [react()]
  
 });
+
